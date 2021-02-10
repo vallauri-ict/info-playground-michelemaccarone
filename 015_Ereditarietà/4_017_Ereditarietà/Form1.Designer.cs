9955b815bab985f9668d2abe7a72d47b5d64a686
Michele Maccarone <62659283+michelemaccarone@users.noreply.github.com>
2021-02-10 09:10:44 +0100

    Add ex 15

---
+++ 015_Ereditarietà/4_017_Ereditarietà/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace _4_017_Ereditarietà
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostro = new System.Windows.Forms.Button();
+            this.btnMannaro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMostro
+            // 
+            this.btnMostro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostro.Location = new System.Drawing.Point(65, 27);
+            this.btnMostro.Name = "btnMostro";
+            this.btnMostro.Size = new System.Drawing.Size(189, 58);
+            this.btnMostro.TabIndex = 0;
+            this.btnMostro.Text = "Mostro";
+            this.btnMostro.UseVisualStyleBackColor = true;
+            this.btnMostro.Click += new System.EventHandler(this.btnMostro_Click);
+            // 
+            // btnMannaro
+            // 
+            this.btnMannaro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMannaro.Location = new System.Drawing.Point(260, 27);
+            this.btnMannaro.Name = "btnMannaro";
+            this.btnMannaro.Size = new System.Drawing.Size(189, 58);
+            this.btnMannaro.TabIndex = 1;
+            this.btnMannaro.Text = "Mannaro";
+            this.btnMannaro.UseVisualStyleBackColor = true;
+            this.btnMannaro.Click += new System.EventHandler(this.btnMannaro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 112);
+            this.Controls.Add(this.btnMannaro);
+            this.Controls.Add(this.btnMostro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostro;
+        private System.Windows.Forms.Button btnMannaro;
+    }
+}
+
---
+++ 015_Ereditarietà/4_017_Ereditarietà/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_017_Ereditarietà
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostro_Click(object sender, EventArgs e)
+        {
+            Mostro demone = new Mostro();
+            MessageBox.Show(demone.Valore() + " " + demone.Verso());
+        }
+
+        private void btnMannaro_Click(object sender, EventArgs e)
+        {
+            Mannaro umano = new Mannaro();
+            MessageBox.Show(umano.Valore() + " " + umano.Verso());
+            umano.Muta();
+            MessageBox.Show(umano.Valore() + " " + umano.Verso());
+        }
+    }
+}

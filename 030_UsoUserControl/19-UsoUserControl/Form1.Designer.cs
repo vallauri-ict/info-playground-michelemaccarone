8df9629fea91c5802aa29bd8d1a1db48d7fd0837
Michele Maccarone <62659283+michelemaccarone@users.noreply.github.com>
2021-04-06 11:02:25 +0200

    Add files via upload

---
+++ 030_UsoUserControl/19-UsoUserControl/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _19_UsoUserControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMiaTextBox = new _19_UserControl.UcTextBox();
+            this.chkNumeri = new System.Windows.Forms.CheckBox();
+            this.nudCifreDecimali = new System.Windows.Forms.NumericUpDown();
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.btnPulisci = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCifreDecimali)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMiaTextBox
+            // 
+            this.txtMiaTextBox.CifreDecimali = 0;
+            this.txtMiaTextBox.Location = new System.Drawing.Point(91, 60);
+            this.txtMiaTextBox.Name = "txtMiaTextBox";
+            this.txtMiaTextBox.Numero = false;
+            this.txtMiaTextBox.Size = new System.Drawing.Size(102, 22);
+            this.txtMiaTextBox.TabIndex = 0;
+            this.txtMiaTextBox.Testo = "";
+            // 
+            // chkNumeri
+            // 
+            this.chkNumeri.AutoSize = true;
+            this.chkNumeri.Location = new System.Drawing.Point(40, 112);
+            this.chkNumeri.Name = "chkNumeri";
+            this.chkNumeri.Size = new System.Drawing.Size(123, 17);
+            this.chkNumeri.TabIndex = 1;
+            this.chkNumeri.Text = "Accetta Solo Numeri";
+            this.chkNumeri.UseVisualStyleBackColor = true;
+            this.chkNumeri.CheckedChanged += new System.EventHandler(this.chkNumeri_CheckedChanged);
+            // 
+            // nudCifreDecimali
+            // 
+            this.nudCifreDecimali.Location = new System.Drawing.Point(203, 112);
+            this.nudCifreDecimali.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudCifreDecimali.Name = "nudCifreDecimali";
+            this.nudCifreDecimali.Size = new System.Drawing.Size(40, 20);
+            this.nudCifreDecimali.TabIndex = 2;
+            this.nudCifreDecimali.ValueChanged += new System.EventHandler(this.nudCifreDecimali_ValueChanged);
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(29, 162);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(75, 23);
+            this.btnVisualizza.TabIndex = 3;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // btnPulisci
+            // 
+            this.btnPulisci.Location = new System.Drawing.Point(158, 161);
+            this.btnPulisci.Name = "btnPulisci";
+            this.btnPulisci.Size = new System.Drawing.Size(75, 23);
+            this.btnPulisci.TabIndex = 4;
+            this.btnPulisci.Text = "Pulisci";
+            this.btnPulisci.UseVisualStyleBackColor = true;
+            this.btnPulisci.Click += new System.EventHandler(this.btnPulisci_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnPulisci);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.nudCifreDecimali);
+            this.Controls.Add(this.chkNumeri);
+            this.Controls.Add(this.txtMiaTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudCifreDecimali)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private _19_UserControl.UcTextBox txtMiaTextBox;
+        private System.Windows.Forms.CheckBox chkNumeri;
+        private System.Windows.Forms.NumericUpDown nudCifreDecimali;
+        private System.Windows.Forms.Button btnVisualizza;
+        private System.Windows.Forms.Button btnPulisci;
+    }
+}
+
---
+++ 030_UsoUserControl/19-UsoUserControl/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19_UsoUserControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImpostaProperty(false);
+        }
+
+        private void ImpostaProperty(bool numero)
+        {
+            try
+            {
+                txtMiaTextBox.Pulisci();
+                nudCifreDecimali.Value = 0;
+                chkNumeri.Checked = numero;
+                txtMiaTextBox.Numero = numero;
+                txtMiaTextBox.CifreDecimali = Convert.ToInt32(nudCifreDecimali.Value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Valore inserito: "+txtMiaTextBox.Testo);
+        }
+
+        private void btnPulisci_Click(object sender, EventArgs e)
+        {
+            txtMiaTextBox.Pulisci();
+        }
+
+        private void chkNumeri_CheckedChanged(object sender, EventArgs e)
+        {
+            ImpostaProperty(chkNumeri.Checked);
+        }
+
+        private void nudCifreDecimali_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtMiaTextBox.CifreDecimali = Convert.ToInt32(nudCifreDecimali.Value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
